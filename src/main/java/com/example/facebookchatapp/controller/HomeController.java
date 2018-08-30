85e4376378f155eaf2ac79f59f74c5964ecf0e4f
Shubham Gupta <shubham.h.gupta@impetus.co.in>
2018-08-30 19:51:55 +0530

    4 commit

--- src/main/java/com/example/facebookchatapp/controller/HomeController.java
+++ src/main/java/com/example/facebookchatapp/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.example.facebookchatapp.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -14,8 +17,10 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value="/callback",method=RequestMethod.GET)
-	public String firstUpdate(){
-		return "this is something";
+	
+	public String firstUpdate(@RequestParam Map<String,String> allRequestParams){
+		System.out.println(allRequestParams);
+		return allRequestParams.toString();
 	}
 	
 
