98728f7cb44a8744d0000b57604a7824e98e0a1f
Shubham Gupta <shubham.h.gupta@impetus.co.in>
2018-08-30 19:08:57 +0530

    first commit

---
+++ src/main/java/com/example/facebookchatapp/FacebookchatappApplication.java
@@ -0,0 +1,12 @@
+package com.example.facebookchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookchatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookchatappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/facebookchatapp/FacebookchatappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.facebookchatapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FacebookchatappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
