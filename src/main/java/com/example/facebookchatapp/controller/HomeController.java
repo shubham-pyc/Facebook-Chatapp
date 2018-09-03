f658e535963b775df45a10a4caef467e832e5fa6
Shubham Gupta <shubham.h.gupta@impetus.co.in>
2018-09-03 11:26:51 +0530

    5th commit

--- src/main/java/com/example/facebookchatapp/controller/HomeController.java
+++ src/main/java/com/example/facebookchatapp/controller/HomeController.java
@@ -58,7 +58,7 @@ public class HomeController {
 	public static void sendMessage(String id,Message message) throws IOException{
 		String uri = IConstans.FACEBOOK_URI + IConstans.TOKEN;
 		FacebookClient pageClient = new DefaultFacebookClient(IConstans.TOKEN,Version.VERSION_2_9);
-		SendResponse  response =  pageClient.publish("me/messages", SendResponse.class,
+		SendResponse  response =  pageClient.publish("/callback", SendResponse.class,
 				Parameter.with("recipient", id),
 					Parameter.with("message", message) );
 		
